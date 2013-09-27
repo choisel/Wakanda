a8b7e3f9778ce571cca8c2b6f11f84c3a4634ed7
Choisel Fogang <Chaz@choisel.fritz.box>
2013-09-27 19:54:22 +0200

    Moved folders to submodules

--- CORS/bootStraps/handlers.js
+++
@@ -1,5 +0,0 @@
-﻿/**
-* @author Choisel
-*/
-// Handles all calls to urls starting by /cors/, with the handleCORSMethod from the cors.js file.
-addHttpRequestHandler("^/cors/", "utils/cors.js",  "handleCORSMethod");
